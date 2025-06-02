ed5eb6f36a5dd6285c2e6633b13446dd7a0a7f86
Kokon4 <kicodbz1@gmail.com>
2025-06-02 12:52:38 +0200

    Canvis

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,7 +12,7 @@
         @endsession
 
         <div class="card">
-            <div class="card-header">Product List</div>
+            <div class="card-header">Product List -Editado-</div>
             <div class="card-body">
                 <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Product</a>
                 <table class="table table-striped table-bordered">
